54d4982cd6ed2d1824b580a52dce30f88d84bb1e
jinyi <jinyiwei2012@philia093.ren>
2026-07-01 23:47:12 +0800

    feat(__init__): export MultiSpeakerTTS, SpeakerConfig, SpeakerWeights

--- gsv_tts/__init__.py
+++ gsv_tts/__init__.py
@@ -3,9 +3,14 @@ from .TTS import (
     AudioClip,
     cut_text,
 )
+from .MultiSpeaker import MultiSpeakerTTS
+from .SpeakerWeights import SpeakerConfig, SpeakerWeights
 
 __all__ = [
     "TTS",
+    "MultiSpeakerTTS",
     "AudioClip",
     "cut_text",
+    "SpeakerConfig",
+    "SpeakerWeights",
 ]
